b9593a17fdc5f66c50ec0c721617365ff0b17952
Jan Vorwerk <jan@sportlek.com>
2023-10-21 18:47:29 +0200

    Fixed page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import { SessionProvider } from "./_lib/providers";
 import "./globals.css";
 
-export default async function LocaleLayout(props: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: 'Auth & Intl',
+  description: 'Mini proof of concept mixing Next-Auth + Next Intl with RSC',
+}
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
   return <SessionProvider>{props.children}</SessionProvider>;
 }
